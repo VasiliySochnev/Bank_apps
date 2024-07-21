9789ee67a3e4f7c335b8431cd2600b925557212e
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-07-21 04:58:19 +0300

    add data_conn.py (функция, которая извлекает транзакции из файла xlsx)

---
+++ src/data_conn.py
@@ -0,0 +1,35 @@
+import os
+
+import pandas as pd
+
+from config import DATA_DIR
+
+data_file_patch = os.path.join(DATA_DIR, "operations.xls")
+
+
+def get_transaction_from_xlsx_file(path: str) -> pd.DataFrame:
+    """функция извлекает транзакции из файла xlsx"""
+    try:
+        df = pd.read_excel(path)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File {path} not found")
+
+    not_cancel_op = df.loc[df["Статус"] == "OK"]  # фильтруем некорретные опреации
+    pd.options.mode.chained_assignment = None  # подавление ошибки SettingWithCopyWarning
+    not_cancel_op["datetime_col"] = pd.to_datetime(not_cancel_op["Дата операции"], dayfirst=True)
+    # делаем столбец в формате datatime и сортируем по нему
+    not_cancel_op.sort_values(
+        ["datetime_col"],
+        axis=0,
+        ascending=True,
+        inplace=False,
+        kind="quicksort",
+        na_position="last",
+        ignore_index=True,
+        key=None,
+    )
+    return not_cancel_op
+
+
+def get_dataframe() -> pd.DataFrame:
+    return get_transaction_from_xlsx_file(data_file_patch)
