fa448fc983c9c690ec90f97686cdbb0a6152fcf8
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-07-22 21:56:10 +0300

    add test_reports.py

---
+++ tests/test_reports.py
@@ -0,0 +1,19 @@
+import os
+
+import pandas as pd  # type: ignore
+
+from config import TEST_DIR
+from src.reports import spending_by_category
+
+# import pytest
+
+
+test_date1 = "2021-12-31"
+test_df = pd.read_excel(os.path.join(TEST_DIR, "test_df.xlsx"))
+test_df["datetime_col"] = pd.to_datetime(test_df["Дата операции"], dayfirst=True)
+
+
+def test_spending_by_category():  # type: ignore
+    """тест выборка по дате"""
+    result = spending_by_category(test_df, "Ж/д билеты", test_date1)
+    assert len(result) == 2
